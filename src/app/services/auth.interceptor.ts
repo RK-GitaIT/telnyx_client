c0b989f37ee1f8db4fb049859d7d63a3a48547db
RKGitaIT <rampolineni@gitait.com>
2025-02-27 13:52:07 +0530

    token updated.

--- src/app/services/auth.interceptor.ts
+++ src/app/services/auth.interceptor.ts
@@ -6,7 +6,7 @@ import { config } from '../../config';
 export const AuthInterceptor: HttpInterceptorFn = (req: HttpRequest<any>, next: HttpHandlerFn): Observable<any> => {
   const clonedReq = req.clone({
     setHeaders: {
-      Authorization: `Bearer ${config.apiKey}`,
+      Authorization: `Bearer KEY019545FFDF282A75C08A9F6CA4E7BFB7_70MN4UvpyiZ9qSoq2buqO2`,
       'Content-Type': 'application/json',
       Accept: 'application/json'
     }
--- src/assets/env.js
+++ src/assets/env.js
@@ -1,5 +1,5 @@
 window.env = {
     apiUrl: 'https://api.telnyx.com/v2',
-    authToken: atob('KEY019545FFDF282A75C08A9F6CA4E7BFB7_70MN4UvpyiZ9qSoq2buqO2'),
+    authToken:'KEY019545FFDF282A75C08A9F6CA4E7BFB7_70MN4UvpyiZ9qSoq2buqO2',
   };
   
